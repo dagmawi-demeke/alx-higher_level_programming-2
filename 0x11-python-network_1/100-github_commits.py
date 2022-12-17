dea1999bec07066e1b36cfe61813e1b187e05f0b
dagmawi-demeke <67114822+dagmawi-demeke@users.noreply.github.com>
2022-12-17 12:57:16 +0300

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,14 +1,24 @@
-#!/usr/bin/python3
-""" Time for an interview!
-"""
-from sys import argv
 import requests
+import sys
 
+def main(repo_name, owner_name):
+  # Make a request to the GitHub API to get the repository information
+  url = f'https://api.github.com/repos/{owner_name}/{repo_name}'
+  response = requests.get(url)
 
-if __name__ == "__main__":
-    commit = requests.get(
-        f'https://api.github.com/repos/{argv[1]}/{argv[2]}/commits').json()
-    for i in range(10):
-        print("{}: {}".format(
-            commit[i].get("sha"),
-            commit[i].get("commit").get("author").get("name")))
+  # Check if the rate limit was exceeded
+  if response.status_code == 403:
+    print('Rate limit exceeded. Try again later.')
+    return
+
+  # Print the repository name and description
+  repo_info = response.json()
+  print(f'Repository name: {repo_info["name"]}')
+  print(f'Description: {repo_info["description"]}')
+
+if name == 'main':
+  # Get the repository name and owner name from the command line arguments
+  repo_name = sys.argv[1]
+  owner_name = sys.argv[2]
+
+  main(repo_name, owner_name)
